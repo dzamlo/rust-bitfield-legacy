d84b79792e2b44f4bda15f1cb61a3bcf540b1f2e
Loïc Damien <loic.damien@dzamlo.ch>
2014-11-01 02:40:44 +0100

    Declare the macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
-#[test]
-fn it_works() {
+#![feature(plugin_registrar)]
+
+extern crate syntax;
+extern crate rustc;
+
+use rustc::plugin::Registry;
+use syntax::ast;
+use syntax::codemap::Span;
+use syntax::ext::base::{ExtCtxt, MacResult, DummyResult};
+
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    reg.register_macro("bitfield", expand_bitfield);
+}
+
+fn expand_bitfield(cx: &mut ExtCtxt, sp: Span, args: &[ast::TokenTree])
+        -> Box<MacResult + 'static> {
+    return DummyResult::any(sp);
 }
