af4b92bc22196a54d3800f425acdc2703324f683
Loïc Damien <loic.damien@dzamlo.ch>
2014-11-03 23:43:20 +0100

    Separate the flags field in 3 bool field

--- examples/ipv4.rs
+++ examples/ipv4.rs
@@ -10,7 +10,9 @@ bitfield!(IpV4Header,
     ecn: 3,
     total_length: 16,
     identification: 16,
-    flags: 3,
+    reserved: 1,
+    df: 1,
+    mf: 1,
     fragment_offset: 13,
     time_to_live: 8,
     protocol: 8,
@@ -28,6 +30,8 @@ fn main() {
     assert!(header.get_version() == 4);
     assert!(header.get_total_length() == 64);
     assert!(header.get_identification() == 0x6927);
+    assert!(header.get_df());
+    assert!(!header.get_mf());
     assert!(header.get_fragment_offset() == 0);
     assert!(header.get_protocol() == 0x11);
     assert!(header.get_source_address() == [192, 168, 1, 42]);
