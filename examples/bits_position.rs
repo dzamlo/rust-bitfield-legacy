0ba71aef59549d8a42fe315a2eabade1327a96d6
Loïc Damien <loic.damien@dzamlo.ch>
2014-11-14 16:59:35 +0100

    Add an example
    Add an example to ilustrate the underlying data organisation (bits and byte position).

---
+++ examples/bits_position.rs
@@ -0,0 +1,29 @@
+#![feature(phase)]
+#![allow(dead_code)]
+
+#[phase(plugin)]
+extern crate bitfield;
+
+bitfield!(IpV4Header,
+    field1: 7,
+    field2: 7,
+    field3: 10,
+    )
+
+fn main() {
+    let data = [0, ..3];
+    
+    let mut header = IpV4Header::new(data);
+    header.set_field1(0x7f);
+    println!("Field 1 set to all 1:");
+    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+    header.set_field1(0);
+    header.set_field2(0x7F);
+    println!("Field 2 set to all 1:");
+    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+    header.set_field2(0);
+    header.set_field3(0x3FF);
+    println!("Field 3 set to all 1:");
+    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+}
+
