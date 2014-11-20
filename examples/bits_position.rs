4bb4335d9314440c68149be56dc48972c9afa36d
Loïc Damien <loic.damien@dzamlo.ch>
2014-11-20 23:31:11 +0100

    The format specifier for Boolean has changed

--- examples/bits_position.rs
+++ examples/bits_position.rs
@@ -16,14 +16,14 @@ fn main() {
     let mut header = IpV4Header::new(data);
     header.set_field1(0x7f);
     println!("Field 1 set to all 1:");
-    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+    println!("[{:08b}, {:08b}, {:08b}]", header.data[0], header.data[1], header.data[2])
     header.set_field1(0);
     header.set_field2(0x7F);
     println!("Field 2 set to all 1:");
-    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+    println!("[{:08b} {:08b}, {:08b}]", header.data[0], header.data[1], header.data[2])
     header.set_field2(0);
     header.set_field3(0x3FF);
     println!("Field 3 set to all 1:");
-    println!("[{:08t}, {:08t}, {:08t}]", header.data[0], header.data[1], header.data[2])
+    println!("[{:08b}, {:08b}, {:08b}]", header.data[0], header.data[1], header.data[2])
 }
 
