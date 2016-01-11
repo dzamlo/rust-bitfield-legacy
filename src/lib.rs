892dfe2caa50285ef25d4c3a5e2c22d1bd3fc540
Loïc Damien <loic.damien@dzamlo.ch>
2016-01-11 22:35:24 +0100

    Update for latest nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-#![allow(unstable)]
-#![feature(plugin_registrar, quote)]
+#![crate_type="dylib"]
+#![feature(plugin_registrar, rustc_private, quote)]
 
 extern crate syntax;
 extern crate rustc;
+extern crate rustc_plugin;
 
-use rustc::plugin::Registry;
+use rustc_plugin::Registry;
 use syntax::ast;
+use syntax::ast::{ImplItem};
 use syntax::codemap::{DUMMY_SP, Span};
-use syntax::ext::base::{ExtCtxt, MacItems, MacResult};
+use syntax::ext::base::{ExtCtxt, MacEager, MacResult};
 use syntax::ext::build::AstBuilder;
 use syntax::ext::quote::rt::ToTokens;
 use syntax::parse::common::SeqSep;
 use syntax::parse::parser::Parser;
 use syntax::parse::token;
 use syntax::ptr::P;
+use syntax::util::small_vector::SmallVector;
 
-#[derive(Show)]
+
 enum Field {
    ArrayField {name: String, count: usize, element_length: u8},
-   ScalarField {name: String, length: u8},   
+   ScalarField {name: String, length: u8},
 }
 
 
@@ -29,7 +32,7 @@ impl Field {
           Field::ScalarField{name: _, length} => length as u64,
        }
     }
-    
+
     fn gen_single_value_get_expr(cx: &mut ExtCtxt, value_type: &P<ast::Ty>, start: u64, length: u8) -> P<ast::Expr> {
         if length == 1 {
             let byte_shift = (7-start%8) as usize;
@@ -44,41 +47,41 @@ impl Field {
                 let mask = 0xFFu8 >> (8-can_get) as usize;
                 let byte_shift = (8-can_get-(bit_offset%8) as u8) as usize;
                 let bits_expr = quote_expr!(cx, ((self.data[$index] >> $byte_shift) & $mask) as $value_type);
-                
+
                 value_expr = match value_expr {
                      Some(expr) => {
-                         // ExprParen should be a no-ops but it seem that the to_tokens used by quote_method 
+                         // ExprParen should be a no-ops but it seem that the to_tokens used by quote_method
                          // "flatten" the expression. ExprParen prevent this.
                          let expr = cx.expr(DUMMY_SP, ast::ExprParen(expr));
                          let shifted = cx.expr_binary(DUMMY_SP, ast::BiShl, expr, cx.expr_usize(DUMMY_SP, can_get as usize));
                          Some(cx.expr_binary(DUMMY_SP, ast::BiBitOr, shifted, bits_expr))
                      }
                      None => Some(bits_expr)
-                };   
+                };
                 bits_to_get -= can_get;
                 bit_offset += can_get as u64;
             }
            value_expr.unwrap()
         }
     }
-    
+
     fn gen_single_value_set_stmt(cx: &mut ExtCtxt, type_length: u8, start: u64, length: u8) -> P<ast::Stmt> {
         if length == 1 {
             let mask = 0x1u8 << (7-start%8) as usize;
             let index = (start/8) as usize;
-            quote_stmt!(cx, if value {self.data[$index] |= $mask} 
-                            else {self.data[$index] &= !($mask)})
+            quote_stmt!(cx, if value {self.data[$index] |= $mask}
+                            else {self.data[$index] &= !($mask)}).unwrap()
         } else {
             let mut stmts = Vec::new();
             let mut bits_to_set = length;
             let mut bit_offset = start;
-            
+
             let mut value_shift = (start%8) as isize - (type_length - length) as isize + 8*(type_length/8-1) as isize;
 
             while bits_to_set > 0 {
                 let can_set = std::cmp::min(bits_to_set, 8-(bit_offset%8) as u8);
                 let index = (bit_offset/8) as usize;
-                
+
                 // only bits set to 1 in the mask are modified
                 let mask = (0xFFu8 >> 8-can_set as usize) << (8-can_set-(bit_offset%8) as u8) as usize;
 
@@ -86,16 +89,16 @@ impl Field {
                 // negative value means we want shift to the left
                 if value_shift > 0 {
                     let value_shift = value_shift as usize;
-                    stmts.push(quote_stmt!(cx, self.data[$index] = 
-                        (self.data[$index] & !$mask) | 
-                        ((value >> $value_shift) as u8)& $mask));
+                    stmts.push(quote_stmt!(cx, self.data[$index] =
+                        (self.data[$index] & !$mask) |
+                        ((value >> $value_shift) as u8)& $mask).unwrap());
                 } else {
                     let value_shift = (-value_shift) as usize;
-                    stmts.push(quote_stmt!(cx, self.data[$index] = 
-                        (self.data[$index] & !$mask) | 
-                        ((value << $value_shift) as u8)& $mask));
+                    stmts.push(quote_stmt!(cx, self.data[$index] =
+                        (self.data[$index] & !$mask) |
+                        ((value << $value_shift) as u8)& $mask).unwrap());
                 }
-                
+
                 bits_to_set -= can_set;
                 bit_offset += can_set as u64;
                 value_shift -= 8;
@@ -105,85 +108,98 @@ impl Field {
             cx.stmt_expr(expr)
         }
    }
-    
-   fn to_methods(&self, cx: &mut ExtCtxt, start: u64) -> Vec<P<ast::Method>> {
+
+   fn to_methods(&self, cx: &mut ExtCtxt, struct_ident: ast::Ident, start: u64) -> Vec<P<ast::Item>> {
        let mut methods = vec![];
+
        match *self {
            Field::ArrayField{ref name, count, element_length} => {
+
                let (element_type, value_type_length) = size_to_ty(cx, element_length).unwrap();
                let value_type = make_array_ty(cx, &element_type, count);
-               let getter_name = "get_".to_string() + (*name).as_slice();
-               let getter_ident = token::str_to_ident(getter_name.as_slice());
-               
+               let getter_name = "get_".to_string() + &name[..];
+               let getter_ident = token::str_to_ident(&getter_name[..]);
+
                let mut element_getter_exprs = Vec::with_capacity(count);
-               for i in range(0, count) {
+               for i in 0..count {
                     let element_start = start+(i as u64)*(element_length as u64);
                     element_getter_exprs.push(
                         Field::gen_single_value_get_expr(cx, &element_type, element_start, element_length)
                     );
                }
-               
+
                let getter_expr = cx.expr_vec(DUMMY_SP, element_getter_exprs);
-               
-               let getter = quote_method!(cx,
-                   #[inline]
-                   fn $getter_ident(&self) -> $value_type {
-                      $getter_expr
+
+               let getter = quote_item!(cx,
+                   impl $struct_ident {
+                       #[inline]
+                       fn $getter_ident(&self) -> $value_type {
+                          $getter_expr
+                       }
                    }
-               );
+               ).unwrap();
                methods.push(getter);
 
-               let setter_name = "set_".to_string() + (*name).as_slice();
-               let setter_ident = token::str_to_ident(setter_name.as_slice());
+               let setter_name = "set_".to_string() + &name[..];
+               let setter_ident = token::str_to_ident(&setter_name[..]);
                let mut element_setter_stmts = Vec::new();
-               for i in range(0, count) {
+               for i in 0..count {
                     let element_start = start+(i as u64)*(element_length as u64);
                     let stmt = Field::gen_single_value_set_stmt(cx, value_type_length, element_start, element_length);
                     element_setter_stmts.push(
-                        quote_stmt!(cx, { let value = value[$i]; $stmt})
+                        quote_stmt!(cx, { let value = value[$i]; $stmt}).unwrap()
                     );
                }
-               
-               
-               
+
+
+
                let block = cx.block(DUMMY_SP, element_setter_stmts, None);
                let expr = cx.expr(DUMMY_SP, ast::ExprBlock(block));
                let setter_stmt = cx.stmt_expr(expr);
-               
-               let setter = quote_method!(cx,
-                   #[inline]
-                   fn $setter_ident(&mut self, value: $value_type) {
-                      $setter_stmt
+
+               let setter = quote_item!(cx,
+                   impl $struct_ident {
+                       #[inline]
+                       fn $setter_ident(&mut self, value: $value_type) {
+                          $setter_stmt
+                       }
                    }
-               );
+               ).unwrap();
                methods.push(setter);
-               
+
            },
            Field::ScalarField{ref name, length} => {
+
                let (value_type, value_type_length) = size_to_ty(cx, length).unwrap();
-               let getter_name = "get_".to_string() + (*name).as_slice();
-               let getter_ident = token::str_to_ident(getter_name.as_slice());
+               let getter_name = "get_".to_string() + &name[..];
+               let getter_ident = token::str_to_ident(&getter_name[..]);
                let getter_expr = Field::gen_single_value_get_expr(cx, &value_type, start, length);
-               let getter = quote_method!(cx,
-                   #[inline]
-                   fn $getter_ident(&self) -> $value_type {
-                      $getter_expr
+               let getter = quote_item!(cx,
+                   impl $struct_ident {
+                       #[inline]
+                       fn $getter_ident(&self) -> $value_type {
+                          $getter_expr
+                       }
                    }
-               );
+               ).unwrap();
                methods.push(getter);
-               let setter_name = "set_".to_string() + (*name).as_slice();
-               let setter_ident = token::str_to_ident(setter_name.as_slice());
+
+               let setter_name = "set_".to_string() + &name[..];
+               let setter_ident = token::str_to_ident(&setter_name[..]);
                let setter_stmt = Field::gen_single_value_set_stmt(cx, value_type_length, start, length);
-               let setter = quote_method!(cx,
-                   #[inline]
-                   fn $setter_ident(&mut self, value: $value_type) {
-                      $setter_stmt
+               let setter = quote_item!(cx,
+                   impl $struct_ident {
+                       #[inline]
+                       fn $setter_ident(&mut self, value: $value_type) {
+                          $setter_stmt
+                       }
                    }
-               );
-               methods.push(setter);               
+               ).unwrap();
+               methods.push(setter);
+
            },
        }
-       
+
        methods
     }
 }
@@ -198,7 +214,7 @@ fn size_to_ty(cx: &mut ExtCtxt, size: u8) -> Option<(P<ast::Ty>, u8)> {
           i if i <= 8 => Some((quote_ty!(cx, u8), 8)),
           i if i <= 16 => Some((quote_ty!(cx, u16), 16)),
           i if i <= 32 => Some((quote_ty!(cx, u32), 32)),
-          i if i <= 64 => Some((quote_ty!(cx, u64), 64)),          
+          i if i <= 64 => Some((quote_ty!(cx, u64), 64)),
           _ => None
        }
 }
@@ -207,20 +223,19 @@ fn make_array_ty(cx: &mut ExtCtxt, elements_type: &P<ast::Ty>, length: usize) ->
     quote_ty!(cx, [$elements_type; $length])
 }
 
-
 fn parse_u64(parser: &mut Parser) -> u64 {
-      let lit = parser.parse_lit();
+      let lit = parser.parse_lit().unwrap();
       match lit.node {
           ast::LitInt(n, _) => n,
-          _ => parser.span_fatal(lit.span, "unsigned integer literal expected")
+          _ => parser.span_bug(lit.span, "unsigned integer literal expected")
       }
 }
 
 fn parse_field(parser: &mut Parser) -> Field {
-    let ident = parser.parse_ident();
-    let name = token::get_ident(ident).to_string();
+    let ident = parser.parse_ident().unwrap();
+    let name = ident.name.to_string();
     parser.expect(&token::Colon);
-    if parser.eat(&token::OpenDelim(token::Bracket)) {
+    if parser.eat(&token::OpenDelim(token::Bracket)).unwrap() {
        // ArrayField
        let  element_length = parse_u64(parser);
        if element_length == 0 || element_length > 64 {
@@ -249,48 +264,42 @@ fn parse_field(parser: &mut Parser) -> Field {
 
 fn expand_bitfield(cx: &mut ExtCtxt, _sp: Span, tts: &[ast::TokenTree])
         -> Box<MacResult + 'static> {
+
     let mut parser = cx.new_parser_from_tts(tts);
-    let struct_ident = parser.parse_ident();
+    let struct_ident = parser.parse_ident().unwrap();
+
     parser.expect(&token::Comma);
-    
+
     let sep = SeqSep {
         sep: Some(token::Comma),
         trailing_sep_allowed: true,
     };
-    
-    let fields = parser.parse_seq_to_end(&token::Eof, sep, |p| parse_field(p));
 
+    let fields = parser.parse_seq_to_end(&token::Eof, sep, |p| Ok(parse_field(p))).unwrap();
+
+    let mut items  = Vec::with_capacity(fields.len()*2+2);
     let bit_length = fields.iter().fold(0, |a, b| a + b.bit_len());
     let byte_length = ((bit_length+7)/8) as usize;
-    
     let struct_decl = quote_item!(cx, struct $struct_ident { data: [u8; $byte_length]};).unwrap();
-     
-    let mut methods = Vec::with_capacity(fields.len()*2+1);
-    
-    methods.push(ast::MethodImplItem(quote_method!(cx, 
-        fn new(data: [u8; $byte_length]) -> $struct_ident { 
-            $struct_ident { data: data}
-        })));
+    items.push(struct_decl);
+
+   let method_new = quote_item!(cx,
+       impl $struct_ident {
+           fn new(data: [u8; $byte_length]) -> $struct_ident {
+               $struct_ident { data: data}
+           }
+        }
+    ).unwrap();
+    items.push(method_new);
 
     let mut field_start = 0;
     for field in fields.iter() {
-        methods.extend(field.to_methods(cx, field_start).into_iter().map(|m| ast::MethodImplItem(m)));
+        items.extend(field.to_methods(cx, struct_ident, field_start));
         field_start += field.bit_len();
     }
-    
-    let struct_impl_tpl = quote_item!(cx, impl $struct_ident { }).unwrap();
-    
-    let node = struct_impl_tpl.node.clone();
-    
-    //Put the methods we generated inside the impl block.
-    let node = match node {
-        ast::ItemImpl(a, b, c, d, e, _) => ast::ItemImpl(a, b, c, d, e, methods),
-        _ => unreachable!()
-    };
-    
-    let struct_impl = cx.item(DUMMY_SP, struct_ident, vec![], node);
-    
-    return MacItems::new(vec![struct_decl, struct_impl].into_iter());
+
+    let s = SmallVector::many(items);
+    return MacEager::items(s);
 }
 
 #[plugin_registrar]
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,5 @@
 #![feature(plugin)]
-
-#[plugin]
-extern crate bitfield;
+#![plugin(bitfield)]
 
 use std::{u8, u16, u32, u64};
 
@@ -14,7 +12,7 @@ bitfield!{BitfiedlTestStruct1,
     unaligned_u8: 8,
     unaligned_u16: 16,
     unaligned_u32: 32,
-    unaligned_u64: 64, 
+    unaligned_u64: 64,
     unaligned_bool: 1,
     unaligned_2 : 2,
     unaligned_3 : 3,
@@ -26,24 +24,24 @@ bitfield!{BitfiedlTestStruct1,
     unaligned_33 : 33,
     unaligned_63 : 63,
     }
-    
+
 #[test]
 fn get_set_aligned() {
    let data = [0; 53];
    let mut foo = BitfiedlTestStruct1::new(data);
-   
+
    assert!(foo.get_aligned_u8() == 0);
    foo.set_aligned_u8(u8::MAX);
    assert!(foo.get_aligned_u8() == u8::MAX);
-   
+
    assert!(foo.get_aligned_u16() == 0);
    foo.set_aligned_u16(u16::MAX);
    assert!(foo.get_aligned_u16() == u16::MAX);
-   
+
    assert!(foo.get_aligned_u32() == 0);
    foo.set_aligned_u32(u32::MAX);
    assert!(foo.get_aligned_u32() == u32::MAX);
-   
+
    assert!(foo.get_aligned_u64() == 0);
    foo.set_aligned_u64(u64::MAX);
    assert!(foo.get_aligned_u64() == u64::MAX);
@@ -57,19 +55,19 @@ fn get_set_aligned() {
 fn get_set_unaligned_whole_size() {
    let data = [0; 53];
    let mut foo = BitfiedlTestStruct1::new(data);
-   
+
    assert!(foo.get_unaligned_u8() == 0);
    foo.set_unaligned_u8(u8::MAX);
    assert!(foo.get_unaligned_u8() == u8::MAX);
-   
+
    assert!(foo.get_unaligned_u16() == 0);
    foo.set_unaligned_u16(u16::MAX);
    assert!(foo.get_unaligned_u16() == u16::MAX);
-   
+
    assert!(foo.get_unaligned_u32() == 0);
    foo.set_unaligned_u32(u32::MAX);
    assert!(foo.get_unaligned_u32() == u32::MAX);
-   
+
    assert!(foo.get_unaligned_u64() == 0);
    foo.set_unaligned_u64(u64::MAX);
    assert!(foo.get_unaligned_u64() == u64::MAX);
@@ -83,39 +81,39 @@ fn get_set_unaligned_whole_size() {
 fn get_set_unaligned_nonwhole_size(){
    let data = [0; 53];
    let mut foo = BitfiedlTestStruct1::new(data);
-   
+
    assert!(foo.get_unaligned_2() == 0);
    foo.set_unaligned_2(u8::MAX);
    assert!(foo.get_unaligned_2() == 0x3);
-   
+
    assert!(foo.get_unaligned_3() == 0);
    foo.set_unaligned_3(u8::MAX);
    assert!(foo.get_unaligned_3() == 0x7);
-   
+
    assert!(foo.get_unaligned_7() == 0);
    foo.set_unaligned_7(u8::MAX);
    assert!(foo.get_unaligned_7() == 0x7F);
-   
+
    assert!(foo.get_unaligned_9() == 0);
    foo.set_unaligned_9(u16::MAX);
    assert!(foo.get_unaligned_9() == 0x1FF);
-   
+
    assert!(foo.get_unaligned_15() == 0);
    foo.set_unaligned_15(u16::MAX);
    assert!(foo.get_unaligned_15() == 0x7FFF);
-   
+
    assert!(foo.get_unaligned_17() == 0);
    foo.set_unaligned_17(u32::MAX);
    assert!(foo.get_unaligned_17() == 0x1FFFF);
-   
+
    assert!(foo.get_unaligned_31() == 0);
    foo.set_unaligned_31(u32::MAX);
    assert!(foo.get_unaligned_31() == 0x7FFFFFFF);
-   
+
    assert!(foo.get_unaligned_33() == 0);
    foo.set_unaligned_33(u64::MAX);
    assert!(foo.get_unaligned_33() == 0x1FFFFFFFF);
-   
+
    assert!(foo.get_unaligned_63() == 0);
    foo.set_unaligned_63(u64::MAX);
    assert!(foo.get_unaligned_63() == 0x7FFFFFFFFFFFFFFF);
@@ -142,24 +140,24 @@ bitfield!{BitfiedlTestStruct2,
     unaligned_33 : [33; 3],
     unaligned_63 : [63; 3],
     }
-    
+
 #[test]
 fn get_set_aligned_array() {
    let data = [0; 159];
    let mut foo = BitfiedlTestStruct2::new(data);
-   
+
    assert!(foo.get_aligned_u8() == [0, 0, 0]);
    foo.set_aligned_u8([42, 0, u8::MAX]);
    assert!(foo.get_aligned_u8() == [42, 0, u8::MAX]);
-   
+
    assert!(foo.get_aligned_u16() == [0, 0, 0]);
    foo.set_aligned_u16([42, 0, u16::MAX]);
    assert!(foo.get_aligned_u16() == [42, 0, u16::MAX]);
-   
+
    assert!(foo.get_aligned_u32() == [0, 0, 0]);
    foo.set_aligned_u32([42, 0, u32::MAX]);
    assert!(foo.get_aligned_u32() == [42, 0, u32::MAX]);
-   
+
    assert!(foo.get_aligned_u64() == [0, 0, 0]);
    foo.set_aligned_u64([42, 0, u64::MAX]);
    assert!(foo.get_aligned_u64() == [42, 0, u64::MAX]);
@@ -173,19 +171,19 @@ fn get_set_aligned_array() {
 fn get_set_unaligned_whole_size_array() {
    let data = [0; 159];
    let mut foo = BitfiedlTestStruct2::new(data);
-   
+
    assert!(foo.get_unaligned_u8() == [0, 0, 0]);
    foo.set_unaligned_u8([42, 0, u8::MAX]);
    assert!(foo.get_unaligned_u8() == [42, 0, u8::MAX]);
-   
+
    assert!(foo.get_unaligned_u16() == [0, 0, 0]);
    foo.set_unaligned_u16([42, 0, u16::MAX]);
    assert!(foo.get_unaligned_u16() == [42, 0, u16::MAX]);
-   
+
    assert!(foo.get_unaligned_u32() == [0, 0, 0]);
    foo.set_unaligned_u32([42, 0, u32::MAX]);
    assert!(foo.get_unaligned_u32() == [42, 0, u32::MAX]);
-   
+
    assert!(foo.get_unaligned_u64() == [0, 0, 0]);
    foo.set_unaligned_u64([42, 0, u64::MAX]);
    assert!(foo.get_unaligned_u64() == [42, 0, u64::MAX]);
@@ -199,39 +197,39 @@ fn get_set_unaligned_whole_size_array() {
 fn get_set_unaligned_nonwhole_size_array(){
    let data = [0; 159];
    let mut foo = BitfiedlTestStruct2::new(data);
-   
+
    assert!(foo.get_unaligned_2() == [0, 0, 0]);
    foo.set_unaligned_2([42, 0, u8::MAX]);
    assert!(foo.get_unaligned_2() == [0x2, 0, 0x3]);
-   
+
    assert!(foo.get_unaligned_3() == [0, 0, 0]);
    foo.set_unaligned_3([42, 0, u8::MAX]);
    assert!(foo.get_unaligned_3() == [0x2, 0, 0x7]);
-   
+
    assert!(foo.get_unaligned_7() == [0, 0, 0]);
    foo.set_unaligned_7([42, 0, u8::MAX]);
    assert!(foo.get_unaligned_7() == [42, 0, 0x7F]);
-   
+
    assert!(foo.get_unaligned_9() == [0, 0, 0]);
    foo.set_unaligned_9([42, 0, u16::MAX]);
    assert!(foo.get_unaligned_9() == [42, 0, 0x1FF]);
-   
+
    assert!(foo.get_unaligned_15() == [0, 0, 0]);
    foo.set_unaligned_15([42, 0, u16::MAX]);
    assert!(foo.get_unaligned_15() == [42, 0, 0x7FFF]);
-   
+
    assert!(foo.get_unaligned_17() == [0, 0, 0]);
    foo.set_unaligned_17([42, 0, u32::MAX]);
    assert!(foo.get_unaligned_17() == [42, 0, 0x1FFFF]);
-   
+
    assert!(foo.get_unaligned_31() == [0, 0, 0]);
    foo.set_unaligned_31([42, 0, u32::MAX]);
    assert!(foo.get_unaligned_31() == [42, 0, 0x7FFFFFFF]);
-   
+
    assert!(foo.get_unaligned_33() == [0, 0, 0]);
    foo.set_unaligned_33([42, 0, u64::MAX]);
    assert!(foo.get_unaligned_33() == [42, 0, 0x1FFFFFFFF]);
-   
+
    assert!(foo.get_unaligned_63() == [0, 0, 0]);
    foo.set_unaligned_63([42, 0, u64::MAX]);
    assert!(foo.get_unaligned_63() == [42, 0, 0x7FFFFFFFFFFFFFFF]);
@@ -240,7 +238,7 @@ fn get_set_unaligned_nonwhole_size_array(){
 bitfield!{BitfiedlTestStruct3,
     byte: [1; 8],
     }
-    
+
 #[test]
 fn bits_pos() {
     let data = [0];
@@ -323,23 +321,23 @@ bitfield!{BitfiedlTestStruct7,
 fn other_literal_form_for_fields_size() {
    let data = [0; 15];
    let mut foo = BitfiedlTestStruct7::new(data);
-   
+
    assert!(foo.get_field1() == 0x0);
    foo.set_field1(u16::MAX);
    assert!(foo.get_field1() == u16::MAX);
-   
+
    assert!(foo.get_field2() == 0x0);
    foo.set_field2(u8::MAX);
    assert!(foo.get_field2() == u8::MAX);
-   
+
    assert!(foo.get_field3() == 0x0);
    foo.set_field3(u8::MAX);
    assert!(foo.get_field3() == 0x3);
-   
+
    assert!(foo.get_field4() == 0x0);
    foo.set_field4(u64::MAX);
    assert!(foo.get_field4() == 0xFFFFFFFFFFFFFFF);
-   
+
    assert!(foo.get_field5() == [0, 0, 0]);
    foo.set_field5([42, 0, u16::MAX]);
    assert!(foo.get_field5() == [42, 0, 0x3FF]);
